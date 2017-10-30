433a8d79a45fa479226ddcc1033c527f8a9364bd
beactivera <weronika.wdowiak56@gmail.com>
2017-10-30 14:38:36 +0100

    improving

--- script.js
+++ script.js
@@ -37,23 +37,28 @@ function showScene01(){
 function showScene2() {
 	scene2.classList.remove("notshowing");
 	scene01.classList.add("notshowing");
-
+	
 	document.getElementById('es-dancing').classList.remove("notshowing");
 	document.getElementById('es-standing').classList.add("notshowing");
 
 	let danceClick = 0
-	esDancing.addEventListener('click', startDancingAudio)
-
-	function startDancingAudio() {
-	if (danceClick == 0) {
-	danceClick ++
-	let dancingAudio = new Audio('audio/2_dance.mp3') //selects document
+	esDancing.addEventListener('click', counterDanceClick)
+	
+	function counterDanceClick(){
+		if (danceClick == 0) {
+    danceClick++
+	
+    let hint2 = document.querySelector(".hint2");
+	hint2.classList.add("notshowing");
+	
+    let dancingAudio = new Audio('audio/2_dance.mp3') //selects document
 	dancingAudio.play() //starts music
 
-
+	
 	setTimeout(showScene02, 11000)
 	}
 	}
+	
 
 	function showScene02(){
 
@@ -69,22 +74,37 @@ function showScene2() {
 		scene02.classList.add("notshowing");
 
 		let hunchback_chasing =document.querySelector(".hunchback");
+		let hunchback_standing =document.querySelector(".hunchback_standing");
+		let esmeralda_standing =document.querySelector(".esmeralda_standing");
+		
+		hunchback_chasing.classList.add("notshowing");
+		esmeralda.classList.add("notshowing");
         
 		let screamClick = 0
 		hunchback_chasing.addEventListener('click', startScream)
-
-		function startScream(){
-        if(screamClick == 0){
-			screamClick++;
+	
 		
-			console.log('scream audio');
+		function startScream(){
+			
+		hunchback_standing.classList.add("notshowing");
+		hunchback_chasing.classList.remove("notshowing");
+		esmeralda_standing.classList.add("notshowing");
+		esmeralda.classList.remove("notshowing");
+			
+        if(screamClick == 0){
+		screamClick++;
+			
+		
+			
+		console.log('scream audio');
 		let screamNewAudio = new Audio('audio/7_murderscream.mp3');
 		screamNewAudio.play();
+		
 
 			setTimeout(chasing, 6000)
 		}
 		}
-
+	
 		function chasing(){
 
         console.log('chase audio');
